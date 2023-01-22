91ba3a449d7d022077f492262332a7aca468073a
Dmitrii Zolotuhin <dmitrii.zolotuhin@agmatix.com>
2023-01-22 16:32:06 +0700

    Init CI

--- apps/web/specs/index.spec.tsx
+++ apps/web/specs/index.spec.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { render } from '@testing-library/react'
+import Page from '../app/page'
+import { gql } from '../data-access/graphq-client'
 
-import Index from '../pages/index'
+gql.GetUsers = jest.fn().mockResolvedValue({ users: [] })
 
 describe('Index', () => {
-  it('should render successfully', () => {
-    const { baseElement } = render(<Index />)
+  it('should render successfully', async () => {
+    const { baseElement } = render((await Page()) as unknown as React.ReactElement)
     expect(baseElement).toBeTruthy()
   })
 })
--- libs/api/feature-user/src/lib/user.resolver.spec.ts
+++ libs/api/feature-user/src/lib/user.resolver.spec.ts
@@ -1,13 +1,14 @@
 import { Test, TestingModule } from '@nestjs/testing'
 import { UserResolver } from './user.resolver'
 import { UserService } from './user.service'
+import { PrismaService } from '@okkino/api/data-access-db'
 
 describe('UserResolver', () => {
   let resolver: UserResolver
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UserResolver, UserService]
+      providers: [UserResolver, UserService, PrismaService]
     }).compile()
 
     resolver = module.get<UserResolver>(UserResolver)
