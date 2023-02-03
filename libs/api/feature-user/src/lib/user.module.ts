726c282ed1e4f167c414d3e4d117f652eb62dac7
Dmitrii Zolotuhin <dmitrii.zolotuhin@agmatix.com>
2023-02-03 22:02:03 +0200

    Init CI

--- libs/api/feature-user/src/lib/user.module.ts
+++ libs/api/feature-user/src/lib/user.module.ts
@@ -1,7 +1,7 @@
-import { Module } from '@nestjs/common';
-import { UserService } from './user.service';
-import { UserResolver } from './user.resolver';
-import {PrismaModule} from "@okkino/api/data-access-db";
+import { Module } from '@nestjs/common'
+import { UserService } from './user.service'
+import { UserResolver } from './user.resolver'
+import { PrismaModule } from '@okkino/api/data-access-db'
 
 @Module({
   providers: [UserResolver, UserService],
