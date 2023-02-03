c4791450d0a79c2f0a3b23a2516f06f13604f205
Dmitrii Zolotuhin <dmitrii.zolotuhin@agmatix.com>
2023-02-03 21:56:08 +0200

    Init CI

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,23 +1,22 @@
-import { gql } from '../data-access/graphq-client';
+import { gql } from '../data-access/graphq-client'
 
 export default async function Page() {
-  const { users } = await gql.GetUsers();
+  const { users } = await gql.GetUsers()
 
   return users.map((user) => (
-    <figure key={user.id} className="bg-slate-100 rounded-xl p-8 dark:bg-slate-800 text-white m-6">
+    <figure key={user.id} className="m-6 rounded-xl bg-slate-100 p-8 text-white dark:bg-slate-800">
       <img
-        className="w-24 h-24 rounded-full mx-auto"
+        className="mx-auto h-24 w-24 rounded-full"
         src={`https://i.pravatar.cc/150?u=${user.id}`}
         alt=""
         width="384"
         height="512"
       />
-      <div className="pt-6 space-y-4">
+      <div className="space-y-4 pt-6">
         <blockquote>
           <p className="text-lg font-medium">
-            “Tailwind CSS is the only framework that I've seen scale on large
-            teams. It’s easy to customize, adapts to any design, and the build
-            size is tiny.”
+            “Tailwind CSS is the only framework that I&apos;ve seen scale on large teams. It’s easy
+            to customize, adapts to any design, and the build size is tiny.”
           </p>
         </blockquote>
         <figcaption>
@@ -26,5 +25,5 @@ export default async function Page() {
         </figcaption>
       </div>
     </figure>
-  ));
+  ))
 }
