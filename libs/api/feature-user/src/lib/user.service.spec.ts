b8aae9bc8bc64c39c08a7201e87ff1bd8644e2b7
Dmitrii Zolotuhin <dmitrii.zolotuhin@agmatix.com>
2023-02-04 11:56:17 +0200

    Init CI

--- libs/api/feature-user/src/lib/user.service.spec.ts
+++ libs/api/feature-user/src/lib/user.service.spec.ts
@@ -1,12 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing'
 import { UserService } from './user.service'
+import { PrismaService } from '@okkino/api/data-access-db'
 
 describe('UserService', () => {
   let service: UserService
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UserService]
+      providers: [UserService, PrismaService]
     }).compile()
 
     service = module.get<UserService>(UserService)
