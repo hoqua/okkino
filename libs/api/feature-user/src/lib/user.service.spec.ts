ef68336a8abe88e05263ea42aa3d7c523f829e68
Dmitrii Zolotuhin <dmitrii.zolotuhin@agmatix.com>
2023-02-11 11:20:14 +0200

    Init CI

--- libs/api/feature-user/src/lib/user.service.spec.ts
+++ libs/api/feature-user/src/lib/user.service.spec.ts
@@ -1,18 +1,19 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { UserService } from './user.service';
+import { Test, TestingModule } from '@nestjs/testing'
+import { UserService } from './user.service'
+import { PrismaService } from '@okkino/api/data-access-db'
 
 describe('UserService', () => {
-  let service: UserService;
+  let service: UserService
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UserService],
-    }).compile();
+      providers: [UserService, PrismaService]
+    }).compile()
 
-    service = module.get<UserService>(UserService);
-  });
+    service = module.get<UserService>(UserService)
+  })
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
-});
+    expect(service).toBeDefined()
+  })
+})
