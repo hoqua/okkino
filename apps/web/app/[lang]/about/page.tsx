da9cf7a5ee5ea9d033e516644ea4a22fe896c7e1
DistoRRRtion <distorrrtion@gmail.com>
2023-06-29 13:08:18 +0300

    add bottom distance

--- apps/web/app/[lang]/about/page.tsx
+++ apps/web/app/[lang]/about/page.tsx
@@ -6,19 +6,17 @@ export default async function About({ params }: { params: { lang: Locale } }) {
   const t = await getDictionary(params.lang)
 
   return (
-    <div className="max-md flex justify-between max-lg:flex-wrap">
-      <div className="relative aspect-square w-full max-w-3xl">
-        <Image
-          className={'absolute left-0 top-0 z-[-1] lg:block'}
-          src="/static-images/about.webp"
-          alt="about"
-          fill
-          priority
-        />
+    // max-lg:grid-cols-1
+    // <div className="max-md flex justify-between max-lg:flex-wrap">
+    <div className="grid gap-10 lg:grid-cols-[2.1fr_2fr]">
+      <div className=" relative aspect-[1/1] lg:max-w-xl 2xl:max-w-3xl ">
+        <Image src="/static-images/about.webp" alt="about" fill priority />
       </div>
-      <div className=" ml-32 mt-10 flex max-w-sm flex-col justify-end max-lg:ml-0 max-lg:max-w-full">
-        <h1 className="mb-3 font-bold uppercase">{t.about.title}</h1>
-        <p className=" text-xs leading-5 text-gray-600">{t.about.text}</p>
+      <div className="flex items-end justify-end">
+        <div className="w-[430px] max-xl:w-full">
+          <h1 className="font-semibold uppercase">{t.about.title}</h1>
+          <p className="text-xs leading-5 text-gray-600">{t.about.text}</p>
+        </div>
       </div>
     </div>
   )
