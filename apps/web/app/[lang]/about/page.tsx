861b015a898b24c377b030299d1ee8e20eb10cf6
DistoRRRtion <distorrrtion@gmail.com>
2023-06-29 15:26:18 +0300

    minor updates

--- apps/web/app/[lang]/about/page.tsx
+++ apps/web/app/[lang]/about/page.tsx
@@ -6,7 +6,7 @@ export default async function About({ params }: { params: { lang: Locale } }) {
   const t = await getDictionary(params.lang)
 
   return (
-    <div className="3xl:grid-cols-[5fr_4fr] grid justify-items-center gap-10 xl:grid-cols-[4fr_5fr]">
+    <div className="3xl:grid-cols-[5fr_4fr] grid gap-10 xl:grid-cols-[4fr_5fr]">
       <div className="relative aspect-square w-full">
         <Image src="/static-images/about.webp" alt="about" fill priority />
       </div>
