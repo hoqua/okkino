98015baed080fea9b2bdc3ec56dbb6a65ada2b1d
Dmitrii Zolotuhin <dmitrii.zolotuhin@agmatix.com>
2023-08-12 19:02:36 +0300

    fixed admin product name

--- apps/web-admin/app/dashboard/product/[name]/page.tsx
+++ apps/web-admin/app/dashboard/product/[name]/page.tsx
@@ -2,7 +2,7 @@ import ProductForm from '../_components/form'
 import { getProduct } from '@okkino/api/data-access-db'
 
 export default async function ProductEdit({ params }: { params: { name: string } }) {
-  const product = await getProduct(params.name)
+  const product = await getProduct(decodeURI(params.name))
 
   return <ProductForm product={product} />
 }
