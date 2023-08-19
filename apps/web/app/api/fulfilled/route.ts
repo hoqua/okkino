0097761097943ac1618567f06ecdfc14aecb4875
Dmitrii Zolotuhin <dmitrii.zolotuhin@agmatix.com>
2023-08-19 20:07:36 +0300

    fixed build error

--- apps/web/app/api/fulfilled/route.ts
+++ apps/web/app/api/fulfilled/route.ts
@@ -35,17 +35,19 @@ export async function POST(req: NextRequest) {
 
     // Handle the checkout.session.completed event
     if (event.type === 'checkout.session.completed') {
+      const { id, shipping, amount_total, customer_details } = event.data.object as any
       await fulfillOrder(
-        event.data.object.id,
-        event.data.object.shipping.address,
-        event.data.object.shipping.name,
-        event.data.object.amount_total / 100,
-        event.data.object.customer_details.email
+        id,
+        shipping.address,
+        shipping.name,
+        amount_total / 100,
+        customer_details.email
       )
     }
   } catch (error) {
+    const { customer_details } = event?.data.object as any
     Sentry.captureException(error, {
-      user: event?.data.object.customer_details.email
+      user: customer_details.email
     })
   }
 
