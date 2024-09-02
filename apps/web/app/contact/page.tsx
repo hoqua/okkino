bc89aad72b632cef487edf8e581c257ec11e765a
Gurin Mihail <73294844+MikeXado@users.noreply.github.com>
2024-09-02 16:41:00 +0300

    Fix/contact page UI (#35)
    * ⬆️ Dependencies: Update & fix some syntax issues
    
    * update contact page ui
    
    * update delivery estimate days

--- apps/web/app/contact/page.tsx
+++ apps/web/app/contact/page.tsx
@@ -4,34 +4,33 @@ import React from 'react'
 
 export default function Page() {
   return (
-    <div className="3xl:grid-cols-[5fr_4fr] xl:gap-40 xl:grid xl:grid-cols-[4fr_5fr] xl:text-left text-center flex flex-col gap-10 items-center uppercase pb-10">
+    <div className="grid gap-10 uppercase text-xs pb-10 3xl:grid-cols-[5fr_4fr] xl:grid-cols-[4fr_5fr]">
       <div className="relative aspect-square w-full">
         <Image src="/static-images/contact.webp" alt="okkino notebook" fill priority />
       </div>
-      <div className="flex flex-col gap-16 justify-between h-full">
-        <div className="mt-auto flex flex-col gap-16">
-          <div className="flex flex-col gap-6">
-            <h2>You are welcome to visit our studio by appointment.</h2>
-            <p>Alexandru cel Bun street 38, chisinau, moldova</p>
-          </div>
+      <div className="flex w-full  justify-center  text-center xl:justify-end xl:text-left">
+        <div className="flex flex-col justify-between gap-16">
+          <div className="mt-auto flex flex-col gap-16">
+            <div className="flex flex-col gap-6">
+              <h2>You are welcome to visit our studio by appointment.</h2>
+              <p>Alexandru cel Bun street 38, chisinau, moldova</p>
+            </div>
 
-          <div className="flex flex-col gap-6">
-            <h3>Contact Us</h3>
-            <div>
-              <Link className="underline" href="mailto:okkino.studio@gmail.com">
-                okkino.studio@gmail.com
-              </Link>
-              <p>Phone (+373) 60 622 211</p>
+            <div className="flex flex-col gap-6">
+              <h3>Contact Us</h3>
+              <div>
+                <Link className="okkino-text-hover" href="mailto:contact@studiookkino.com">
+                  contact@studiookkino.com
+                </Link>
+                <p>Phone (+373) 60 622 211</p>
+              </div>
             </div>
           </div>
-        </div>
-        <div className="mt-auto">
-          <Link
-            className="underline"
-            href="https://www.instagram.com/okkino.studio?igsh=MWFjam1teGdjenBqYw=="
-          >
-            INSTAGRAM
-          </Link>
+          <div className="mt-auto okkino-text-hover">
+            <Link href="https://www.instagram.com/okkino.studio?igsh=MWFjam1teGdjenBqYw==">
+              Instagram
+            </Link>
+          </div>
         </div>
       </div>
     </div>
