053642c72c20bd4eeaa4d2b4a3ce9b82bd514d59
MikeXado <mike.gurin21@gmail.com>
2025-01-16 17:56:17 +0200

    add product image zoom

---
+++ apps/web/app/product/[productUrlName]/components/product-images.tsx
@@ -0,0 +1,42 @@
+'use client'
+import { hexToDataUrl } from '@okkino/web/utils-shared'
+import type { Image as ProductImageData } from '@prisma/client'
+
+import Image from 'next/image'
+import React, { useState } from 'react'
+import ZoomImageModal from './zoom-img-modal'
+
+const IMAGES_ON_SCREEN = 2
+
+export default function ProductImages({ productImages }: { productImages: ProductImageData[] }) {
+  const [zoomImage, setZoomImage] = useState(null)
+  return (
+    <>
+      {productImages.map((image, index) => {
+        return (
+          <section
+            key={image.id}
+            onClick={() => setZoomImage(image)}
+            className={
+              'xl:min-w-full ' + 'relative aspect-[120/179] min-w-[80%] snap-center zoom-cursor'
+            }
+          >
+            <Image
+              src={image.url}
+              alt={image.title}
+              className=""
+              placeholder="blur"
+              blurDataURL={hexToDataUrl(image.bgColor)}
+              title={image.title}
+              priority={index < IMAGES_ON_SCREEN}
+              sizes="(min-width: 1600px) 540px, (min-width: 1280px) calc(28.33vw - 27px), 77.29vw"
+              fill
+            />
+          </section>
+        )
+      })}
+
+      {zoomImage && <ZoomImageModal img={zoomImage} setZoomImage={setZoomImage} />}
+    </>
+  )
+}
