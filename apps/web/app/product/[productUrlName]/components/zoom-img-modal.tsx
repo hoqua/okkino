79fc09c4d47428e4c69f01cc511fedef8e8b0400
MikeXado <mike.gurin21@gmail.com>
2025-01-17 12:15:35 +0200

    make zoom modal full width

--- apps/web/app/product/[productUrlName]/components/zoom-img-modal.tsx
+++ apps/web/app/product/[productUrlName]/components/zoom-img-modal.tsx
@@ -1,5 +1,3 @@
-'use client'
-
 import { Image as ProductImageData } from '@prisma/client'
 import React, { useEffect, useRef, useState } from 'react'
 import 'zoomist/css'
@@ -15,16 +13,13 @@ export default function ZoomImageModal({
   setZoomImage: (props: ProductImageData | null) => void
 }) {
   const zoomistRef = useRef<HTMLDivElement>(null)
-  const containerRef = useRef<HTMLDivElement>(null)
 
   const [imageLoaded, setImageLoaded] = useState(false)
 
   useEffect(() => {
     let zoomistInstance: Zoomist | null = null
     if (imageLoaded && zoomistRef.current) {
-      zoomistInstance = new Zoomist(zoomistRef.current, {
-        zoomer: true,
-      })
+      zoomistInstance = new Zoomist(zoomistRef.current)
     }
 
     return () => {
@@ -34,29 +29,37 @@ export default function ZoomImageModal({
     }
   }, [imageLoaded])
 
-  const handleBackdropClick = (e: React.MouseEvent) => {
-    if (containerRef.current && e.target === containerRef.current) {
-      setZoomImage(null)
-    }
-  }
-
   return (
-    <div
-      ref={containerRef}
-      onClick={handleBackdropClick}
-      className="fixed inset-0 bg-black flex items-center bg-opacity-50"
-    >
-      <div className="w-full max-w-4xl mx-auto p-4 bg-white rounded-md">
-        <div
-          ref={zoomistRef}
-          className="relative overflow-hidden max-h-[80vh] mx-auto zoomist-container"
+    <div className="fixed inset-0 bg-black flex items-center bg-opacity-50">
+      <div className="relative w-full h-full bg-white">
+        <button
+          className="absolute right-4 top-4 z-50"
+          onClick={() => {
+            setZoomImage(null)
+          }}
         >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="40"
+            height="40"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          >
+            <path d="M18 6 6 18" />
+            <path d="m6 6 12 12" />
+          </svg>
+        </button>
+        <div ref={zoomistRef} className=" w-full h-full zoomist-container">
           <div className="zoomist-wrapper">
-            <div className="zoomist-image flex items-center justify-center w-full">
+            <div className="zoomist-image flex items-center justify-center">
               <Image
                 src={img.url}
                 alt={img.title}
-                className="max-w-[540px]"
+                className="max-w-lg"
                 placeholder="blur"
                 blurDataURL={hexToDataUrl(img.bgColor)}
                 width={1080}
@@ -66,11 +69,6 @@ export default function ZoomImageModal({
               />
             </div>
           </div>
-        </div>
-        <div className="flex justify-end w-full">
-          <button className="mt-3 text-xs uppercase" onClick={() => setZoomImage(null)}>
-            Back
-          </button>
         </div>
       </div>
     </div>
