4fc9b6532bda2d11bee8de9f14ec8026405328e6
hoqua <your-personal-email>
2026-01-24 10:20:59 +0530

    feat/all-shop-fix

--- apps/web/app/shop/[category]/page.tsx
+++ apps/web/app/shop/[category]/page.tsx
@@ -1,5 +1,8 @@
 import ShopPageInner from '../_components/shop-page-inner'
 
+export { generateStaticParams } from '../_components/shop-page-inner'
+export const dynamicParams = false
+
 export default function ShopPageCategory(props) {
   return ShopPageInner(props)
 }
